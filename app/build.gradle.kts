eda84dc877e710bd8c9c6f4a96d974c1bd0c5226
gkuznetsov <kuznetsov.mdev@gmail.com>
2024-10-24 17:02:35 +0500

    MT-main: configure build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,20 @@
 plugins {
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias (libs.plugins.kotlin.android)
+    alias (libs.plugins.hilt)
+    alias (libs.plugins.protobuf)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "ru.sandbox.minitales"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.sandbox.minitales"
-        minSdk = 27
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -26,41 +23,47 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    debugImplementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation (libs.androidx.compose.navigation)
+
+    //Dagger Hilt
+    implementation(libs.hilt)
+    implementation(libs.androidx.hilt.compose.navigation)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.ktor.client.core)
+
+    implementation(libs.datastore)
+    implementation(libs.protobuf.javalite)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,86 @@
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias (libs.plugins.ktlint) apply false
+    alias (libs.plugins.spotless) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.android.library)apply false
+    alias (libs.plugins.kotlin.serialization) apply false
+    alias (libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+}
+
+fun BaseExtension.defaultConfig() {
+    compileSdkVersion(34)
+
+    defaultConfig {
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtensionVersion.toString()
+//    }
+    packagingOptions() {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+fun PluginContainer.applyDefaultConfig(project: Project) {
+    whenPluginAdded {
+        when (this) {
+            is AppPlugin -> {
+                project.extensions.getByType<AppExtension>().apply{
+                    defaultConfig()
+                }
+            }
+            is LibraryPlugin -> {
+                project.extensions.getByType<LibraryExtension>().apply{
+                    defaultConfig()
+                }
+            }
+            is JavaPlugin -> {
+                project.extensions.getByType<JavaPluginExtension>().apply{
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+    }
+}
+
+subprojects {
+    project.plugins.applyDefaultConfig(project)
+
+    tasks.withType<KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.addAll(
+                listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
+            )
+        }
+    }
 }
