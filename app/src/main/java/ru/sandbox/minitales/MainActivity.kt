3e0d48adb1d9b4dd0f89b8298f2dc236f65d126d
gkuznetsov <kuznetsov.mdev@gmail.com>
2024-10-30 17:12:18 +0500

    MT-auth: add MiniTalesNavHost

--- app/src/main/java/ru/sandbox/minitales/MainActivity.kt
+++ app/src/main/java/ru/sandbox/minitales/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.compose.MiniTalesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,23 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiniTalesTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = "Hello my app")
-                    }
+                    MiniTalesNavHost(navHostController = rememberNavController())
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiniTalesTheme {
-
     }
 }
