1815aafa1442e273ebf500bb5167af09807230cc
gkuznetsov <kuznetsov.mdev@gmail.com>
2024-10-30 17:12:30 +0500

    MT-auth: fix AaptException: Dependent features configured but no package ID was set.

--- features/auth/build.gradle.kts
+++ features/auth/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
