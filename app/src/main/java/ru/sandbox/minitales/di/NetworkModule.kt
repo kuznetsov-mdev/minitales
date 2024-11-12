ea5d5ca39cdf963077a5059a83fa406c3e7d3c5d
gkuznetsov <kuznetsov.mdev@gmail.com>
2024-11-12 16:14:38 +0500

    MT-di: add dagger NetworkModule

---
+++ app/src/main/java/ru/sandbox/minitales/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package ru.sandbox.minitales.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.http.URLProtocol
+import ru.sandbox.minitales.network.MiniTalesHttpClientBuilder
+import ru.sandbox.minitales.network.RequestHandler
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    fun provideHttpClient(): HttpClient =
+        MiniTalesHttpClientBuilder()
+            .protocol(URLProtocol.HTTP)
+            .host("")
+            .build()
+
+    @Provides
+    fun provideRequestHandler(client: HttpClient): RequestHandler = RequestHandler(client)
+}
