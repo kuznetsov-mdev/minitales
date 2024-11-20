22d2877bdf665bf3fd324950319868f7ddbec076
gkuznetsov <kuznetsov.mdev@gmail.com>
2024-11-20 20:35:49 +0500

    MT-storage: modify build.gradle.kts(storage module)

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+dependencies {
+    implementation(libs.kotlin.coroutines)
 }
