62cf66f5f08f11fea62c1e9e75de21611ce0a70b
gkuznetsov <kuznetsov.mdev@gmail.com>
2024-11-21 10:12:12 +0500

    MT-auth: add UserDataUseCase

--- app/src/main/java/ru/sandbox/minitales/di/AuthModule.kt
+++ app/src/main/java/ru/sandbox/minitales/di/AuthModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import ru.sandbox.minitales.auth.data.AuthRepository
 import ru.sandbox.minitales.auth.data.AuthRepositoryImpl
+import ru.sandbox.minitales.auth.data.UserRepository
+import ru.sandbox.minitales.auth.data.UserRepositoryImpl
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -13,4 +15,7 @@ class AuthModule {
 
     @Provides
     fun provideAuthRepository(impl: AuthRepositoryImpl): AuthRepository = impl
+
+    @Provides
+    fun provideUserRepository(impl: UserRepositoryImpl): UserRepository = impl
 }
